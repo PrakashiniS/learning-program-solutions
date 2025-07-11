4a200a18e37866a6c40136a4f30c770131fd2719
PrakashiniS <prakashini004@gmail.com>
2025-07-11 21:26:22 +0530

    Fix: adjusted long file paths

---
+++ Week_3_Spring_Data_JPA_with_Spring_Boot_Hibernate1/1_spring-data-jpa-handson/5_/code/orm-service/src/test/java/com/cognizant/orm_service/OrmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.orm_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
